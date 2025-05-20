0a09c22905fe2dd993802ee482fdcaed0445f9aa
cristia mora <cristianfmorac@gmail.com>
2025-05-20 10:19:35 -0500

    pruebas app

--- App/app/main.py
+++ App/app/main.py
@@ -1,24 +1,18 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-from database import SessionLocal, engine, Base
-import crud, models, schemas
+from . import models, crud, schemas
+from .models import engine, SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Crear las tablas
-Base.metadata.create_all(bind=engine)
-
-# Dependencia de DB
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
-@app.get("/")
-def root():
-    return {"message": "Bienvenido a FastAPI"}
 
 @app.post("/users/", response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
